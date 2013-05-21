8c846c96b761ebd13113c21a4eaa531ece7503f5
dzenanr <dzenanr@gmail.com>
2013-05-21 11:27:42 -0400

    create from spiral 08 of membership_spirals

---
+++ README.md
@@ -0,0 +1,20 @@
+
+# membership
+
+**Categories**: Web UI, web components, model.
+
+**Concepts**: Member.
+
+**Description**: A web application with one concept and several web components.
+Spirals of this application are in 
+[membership_spirals] (https://github.com/dzenanr/membership_spirals).
+
+Neither [dartlero](https://github.com/dzenanr/dartlero) nor 
+[dartling](https://github.com/dzenanr/dartling) is used for a model.
+
+
+
+
+
+
+
---
+++ web/component/member/member_find_change_delete.dart
@@ -0,0 +1,82 @@
+import 'dart:html';
+
+import 'package:membership/membership.dart';
+import 'package:web_ui/web_ui.dart';
+
+import 'member_add.dart';
+
+class MemberFindChangeDelete extends MemberAdd {
+  Member member;
+  Members members;
+
+  find() {
+    LabelElement message = query("#find-change-delete-message");
+    InputElement code = query("#code");
+    InputElement password = query("#password");
+    InputElement firstName = query("#firstName");
+    InputElement lastName = query("#lastName");
+    var error = false;
+    message.text = '';
+    if (code.value.trim() == '') {
+      message.text = 'code is mandatory; ${message.text}';
+      error = true;
+    }
+    if (!error) {
+      member = members.find(code.value);
+      if (member != null) {
+        message.text = 'found';
+        password.value = member.password;
+        firstName.value =  member.firstName;
+        lastName.value =  member.lastName;
+      } else {
+        message.text = 'not found';
+      }
+    }
+  }
+
+  change() {
+    LabelElement message = query("#find-change-delete-message");
+    if (member != null) {
+      InputElement password = query("#password");
+      InputElement firstName = query("#firstName");
+      InputElement lastName = query("#lastName");
+      var error = false;
+      message.text = '';
+      if (password.value.trim() == '') {
+        message.text = 'password is mandatory; ${message.text}';
+        error = true;
+      }
+      if (firstName.value.trim() == '') {
+        message.text = 'first name is mandatory; ${message.text}';
+        error = true;
+      }
+      if (lastName.value.trim() == '') {
+        message.text = 'last name is mandatory; ${message.text}';
+        error = true;
+      }
+      if (!error) {
+        member.password = password.value;
+        member.firstName = firstName.value;
+        member.lastName = lastName.value;
+        message.text = 'changed';
+      }
+    } else {
+      message.text = 'first find a member';
+    }
+  }
+
+  delete() {
+    LabelElement message = query("#find-change-delete-message");
+    if (member != null) {
+      message.text = '';
+      if (members.remove(member)) {
+        message.text = 'deleted';
+      } else {
+        message.text = 'not deleted';
+      }
+    } else {
+      message.text = 'first find a member';
+    }
+  }
+
+}
